723b6f033482c4d286a828b4018f0b5024329a99
Han Hollander <han_hollander@hotmail.nl>
2018-01-17 14:04:48 +0100

    server naem

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ log.log('sensor', 'Init UDP PORT: ' + str(UDP_PORT))
 
 # Create the UDP network handler
 net_hand_udp = NetworkHandlerUDP(UDP_PORT)
-net_hand_udp.setName('Network Handler UDP')
+net_hand_udp.setName('Network Handler UDP Server')
 
 # Start the UDP network handler
 net_hand_udp.start()
