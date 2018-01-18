fe9303dec6b16ed8a9d6df1a695d994aef3f3d76
Wijtse Rekker <wijtse@rekkerit.nl>
2018-01-18 12:36:21 +0100

    fixed /log command

--- src/networking/chinkieHandlerServer.py
+++ src/networking/chinkieHandlerServer.py
@@ -1,7 +1,8 @@
 from threading import Thread
-
 from logger import Logger
 from networking.networkHandlerUDP import NetworkHandlerUDP
+
+import platform
 
 
 class ChinkieHandlerServer(Thread):
@@ -26,7 +27,7 @@ class ChinkieHandlerServer(Thread):
                     else:
                         print('Disabled logger')
             else:
-                packet = self.nwh.protocol.wrap_msg(msg)
+                packet = self.nwh.protocol.wrap_msg(platform.node() + ': ' + msg)
                 self.nwh.multisend(packet)
 
     def rec_msg(self, msg, addr):
