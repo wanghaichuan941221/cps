8929d584037f98cde0c219b4484080632aae664a
Wijtse Rekker <wijtse@rekkerit.nl>
2018-01-19 12:41:36 +0100

    nice logging with user input?

--- src/logger.py
+++ src/logger.py
@@ -7,9 +7,9 @@ class Logger:
 
     def log(self, src, msg):
         if self.log_on:
-            window = curses.initscr()
-            y, x = window.getyx()
-            window.move(y, 0)
+            # window = curses.initscr()
+            # y, x = window.getyx()
+            # window.move(y, 0)
             print('{:<21}'.format(str(time.time())), '{:<24}'.format(src), msg)
-            window.move(y + 1, x)
-            curses.endwin()
+            # window.move(y + 1, x)
+            # curses.endwin()
