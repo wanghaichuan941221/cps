d170c1160a50b2afd0d31cc51a221aa5faa5456a
jelle bakker <j.h.t.bakker@student.utwente.nl>
2018-01-23 13:51:51 +0100

    side view calculations

--- src/control/pixaltoangle.py
+++ src/control/pixaltoangle.py
@@ -10,10 +10,15 @@ def angle(x1,y1,x2,y2,x3,y3):
     omega = math.acos((math.pow(l2,2)+math.pow(l3,2)-math.pow(l1,2))/(2*l2*l3))
     return omega
 
-def get_theta1_setpoint1(pixal_cordinates_top):
-    x4 = (pixal_cordinates_top[4]+pixal_cordinates_top[0])/2
-    y4 = (pixal_cordinates_top[5]+pixal_cordinates_top[1])/2
+def get_xy_between_two_points(x1,y1,x2,y2):
+    x = (x1+x2)/2
+    y = (y1+y2)/2
+    return x,y
 
+def get_theta1_setpoint1(pixal_cordinates_top):
+    x4, y4 = get_xy_between_two_points(pixal_cordinates_top[4],pixal_cordinates_top[5],pixal_cordinates_top[0],pixal_cordinates_top[1])
+
+    # take note of the fact that it if you want to know the angle P2P0P1 you have to fill in P0P2P1 or P0P1P2
     theta1 = angle(x4,y4,pixal_cordinates_top[2],pixal_cordinates_top[3],pixal_cordinates_top[4],pixal_cordinates_top[5])
     if pixal_cordinates_top[3]<pixal_cordinates_top[5]:
         theta1 = -1*theta1
@@ -22,34 +27,25 @@ def get_theta1_setpoint1(pixal_cordinates_top):
     if pixal_cordinates_top[7]<pixal_cordinates_top[5]:
         setpoint1 = -1*setpoint1
     print("theta1, setpoint1", theta1,setpoint1)
+
+
     return theta1,setpoint1
 
-def get_theta234_side1(pixal_cordinates_side1, theta1):
-    x6 = (pixal_cordinates_top[8]+pixal_cordinates_top[0])/2
-    y6 = (pixal_cordinates_top[9]+pixal_cordinates_top[1])/2
-
-    theta2_temp = angle(x6,y6,pixal_cordinates_top[2],pixal_cordinates_top[3],pixal_cordinates_top[8],pixal_cordinates_top[9])
-    theta2 = (1/2)*math.pi
-
-    x7 = (pixal_cordinates_top[2])
-    y7 = pixal_cordinates_top[3] - (pixal_cordinates_top[3]-y6)
 
 
-    theta2_temp = angle(x7,y7,pixal_cordinates_top[2],pixal_cordinates_top[3],pixal_cordinates_top[4],pixal_cordinates_top[5])
+def get_theta234_side1(pixal_cordinates_side1):
+    x6, y6 = get_xy_between_two_points(pixal_cordinates_side1[8],pixal_cordinates_side1[9],pixal_cordinates_side1[0],pixal_cordinates_side1[1])
 
+    # take note of the fact that it if you want to know the angle P2P0P1 you have to fill in P0P2P1 or P0P1P2
+    theta2_temp = angle(x6,y6,pixal_cordinates_side1[2],pixal_cordinates_side1[3],pixal_cordinates_side1[8],pixal_cordinates_side1[9])
+    theta2 = (1/2)*math.pi-theta2_temp
 
+    theta3_temp = angle(pixal_cordinates_side1[2],pixal_cordinates_side1[3],x6,y6,pixal_cordinates_side1[4],pixal_cordinates_side1[5])
+    theta3 = math.pi-theta3_temp
+
+    theta4_temp = angle(pixal_cordinates_side1[4],pixal_cordinates_side1[5], pixal_cordinates_side1[2],pixal_cordinates_side1[3],pixal_cordinates_side1[6],pixal_cordinates_side1[7])
+    theta4 = math.pi-theta4_temp
     return theta2, theta3, theta4
-
-def get_real_distance_from_base_to_object(pixal_coridinates,calibration_distance,object_higth_in_cm):
-    x13 = pixal_cordinates[4] - pixal_cordinates[0]
-    multi = calibration_distance/x13
-    x13_2 = (pixal_cordinates[4] + pixal_cordinates[0])/2
-    y13_2 = (pixal_cordinates[5] + pixal_cordinates[1])/2
-    radius_from_base_to_object =  (math.sqrt(math.pow((pixal_coridinates[6]-x13_2),2)+math.pow((y13_2-pixal_coridinates[7]),2)))*multi
-    object_cordinates = [0]*2
-    object_cordinates[0]=radius_from_base_to_object
-    object_cordinates[1]=object_higth_in_cm
-    return object_cordinates
 
 
 def forward_kinematics(t2, t3, t4):
@@ -67,12 +63,12 @@ def forward_kinematics(t2, t3, t4):
     return [[x1, y1], [x2, y2], [x3, y3]]
 
 
-def get_distance(p1, p2):
-    distance = math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2))
-    return distance
+def get_distance_to_object(pixal_cordinates_top,pixal_cordinates_side1,height):
+    x4, y4 = get_xy_between_two_points(pixal_cordinates_top[4],pixal_cordinates_top[5],pixal_cordinates_top[0],pixal_cordinates_top[1])
 
-def get_distance_between_endeffector_and_object(t2_, t3_, t4_, pixal_coridinates_,calibration_distance_,object_higth_in_cm_):
-    p1_, p2_, p3_ = forward_kinematics(t2_, t3_, t4_)
-    pobject_ = get_real_distance_from_base_to_object(pixal_coridinates_,calibration_distance_,object_higth_in_cm_)
-    distance_ =get_distance(p3_, pobject_)
-    return distance_
+    multi1 = calibration_distance/phytagoras(pixal_cordinates_top[0],pixal_cordinates_top[1],pixal_cordinates_top[4],pixal_cordinates_top[5])/phytagoras(pixal_cordinates_side1[0],pixal_cordinates_side1[1],pixal_cordinates_side1[8],pixal_cordinates_side1[9])
+    x_distance_to_object_in_side1_view = phytagoras(x4,y4,pixal_cordinates_top[6],pixal_cordinates_top[7])*multi1
+    y_distance_to_object_in_side1_view = height
+
+    distance_to_object =  phytagoras(x_distance_to_object_in_side1_view, y_distance_to_object_in_side1_view, pixal_cordinates_side1[6], pixal_cordinates_side1[7])
+    return distance_to_object
