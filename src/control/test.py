2e4b5b1de39fa7f4f3f27ef3ea7a497f2e941a08
jelle bakker <j.h.t.bakker@student.utwente.nl>
2018-01-23 14:31:06 +0100

    side view calculations

--- src/control/test.py
+++ src/control/test.py
@@ -1,4 +1,4 @@
-import pixaltoanagle
+import pixaltoangle
 
 #  pixalcordinates = [0,300,450,450,600,300,450,450]
 #  pixalcordinates = [150,150,450,300,450,450,300,600]
@@ -9,6 +9,6 @@ pixalcordinates_side1 = [0,300,300,200,300,150,300,50,600,300]
 calibration_distance_in_cm = 40
 height_object_in_cm = 5
 
-print("theta1,setpoint1",get_theta1_setpoint1(pixal_cordinates_top))
-print("theta2,theta3,theta4",get_theta234_side1(pixal_cordinates_side1))
-print("distance",get_distance_to_object(pixalcordinates_top,pixalcordinates_side1,calibration_distance_in_cm,height_object_in_cm))
+print("theta1,setpoint1",pixaltoangle.get_theta1_setpoint1(pixal_cordinates_top))
+print("theta2,theta3,theta4",pixaltoangle.get_theta234_side1(pixal_cordinates_side1))
+print("distance",pixaltoangle.get_distance_to_object(pixalcordinates_top,pixalcordinates_side1,calibration_distance_in_cm,height_object_in_cm))
