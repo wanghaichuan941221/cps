ad4be330a025e1be1215b5cd624d0181145bb68c
jelle bakker <j.h.t.bakker@student.utwente.nl>
2018-01-24 10:40:15 +0100

    side view calculations

--- src/control/test.py
+++ src/control/test.py
@@ -8,7 +8,7 @@ pixalcordinates_top = [0,300,450,450,600,300,599,300] # theta1 = (1/4)*pi and se
 
 #  pixalcordinates = [150,450,300,0,450,150,300,600]
 # pixalcordinates_side1 = [0,300,300,200,300,150,300,50,600,300]  # theta2 = 0, theta3 = 0, theta4 = 0
- pixalcordinates_side1 = [0,300,400,300,450,300,599,300,600,300]  # theta2 = 1/2 * pi, theta3 = 0, theta4 = 0
+pixalcordinates_side1 = [0,300,400,300,450,300,599,300,600,300]  # theta2 = 1/2 * pi, theta3 = 0, theta4 = 0
 # pixalcordinates_side1 = [0,300,300,200,300,150,150,150,600,300]  # theta2 = 0, theta3 = 0, theta4 = -1/2 * pi
 # pixalcordinates_side1 = [0,300,375,225,450,150,525,75,600,300]  # theta2 = 1/4 * pi, theta3 = 0, theta4 = 0
 #pixalcordinates_side1 = [0,300,375,225,300,150,375,75,600,300]  # theta2 = 1/4 * pi, theta3 = -1/2 * pi, theta4 = 1/2 * pi
