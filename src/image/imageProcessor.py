61c9df7b8f4e1f2812e8dbfefd69fa63685cd5a6
Wijtse Rekker <wijtse@rekkerit.nl>
2018-01-24 14:41:24 +0100

    integrated everything let the errors commence

---
+++ src/image/imageProcessor.py
@@ -0,0 +1,105 @@
+from threading import Thread
+
+import cv2
+import numpy as np
+import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+# from collection import deque
+
+# green
+from logger import Logger
+from networking.networkHandlerUDP import NetworkHandlerUDP
+
+green_lower = np.array([47, 104, 78])
+green_upper = np.array([152, 255, 255])
+
+# red color
+red_lower = np.array([159, 77, 146])
+red_upper = np.array([178, 255, 255])
+
+yellow_lower = np.array([27, 157, 76])
+yellow_upper = np.array([38, 254, 159])
+
+imgWidth = 640
+imgHeight = 480
+
+
+class ImageProcessor(Thread):
+
+    def __init__(self, nwh: 'NetworkHandlerUDP', log: 'Logger', is_top_view: 'bool'):
+        super().__init__()
+
+        self.nwh = nwh
+        self.log = log
+        self.is_top_view = is_top_view
+
+        self.running = True
+
+        self.log.log('ImageProcessor', 'Opening camera...')
+        self.camera = PiCamera()
+        self.camera.rotation = 180
+        self.camera.resolution = (imgWidth, imgHeight)
+        self.camera.framerate = 32
+        self.rawCapture = PiRGBArray(self.camera, size=(imgWidth, imgHeight))
+
+        time.sleep(0.5)  # give the camera some time to setup
+        self.log.log('ImageProcessor', 'Camera opened')
+
+    def run(self):
+        if self.is_top_view:
+            while self.running:
+                img = self.capture_hsv_image()
+                mask_red = self.filter_hsv_image(img, red_lower, red_upper)
+                mask_yellow = self.filter_hsv_image(img, yellow_lower, yellow_upper)
+                circles = self.find_circles(mask_red)
+                circles_object = self.find_circles(mask_yellow)
+                if len(circles) == 3:
+                    circles.sort()
+                    if len(circles_object) > 0:
+                        points = circles + [circles_object[0]]
+                    else:
+                        points = circles + [(imgWidth, img)]
+                    self.nwh.multisend(self.nwh.protocol.wrap_top_view(self.unzip_list(points)))
+                else:
+                    self.log.log('ImageProcessor', '3 data points are required, found: ' + str(circles))
+        else:
+            while self.running:
+                img = self.capture_hsv_image()
+                mask_green = self.filter_hsv_image(img, green_lower, green_upper)
+                circles = self.find_circles(mask_green)
+                if len(circles) == 5:
+                    self.nwh.multisend(self.nwh.protocol.wrap_side_view(self.unzip_list(circles)))
+                else:
+                    self.log.log('ImageProcessor', '5 data points are required, found: ' + str(circles))
+
+    def capture_hsv_image(self):
+        self.rawCapture.truncate(0)
+        self.camera.capture(self.rawCapture, format="bgr", use_video_port=True)
+        frame = self.rawCapture.array
+        # blurred = cv2.GaussianBlur(frame, (11, 11), 0) TODO maybe do blur
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    def filter_hsv_image(self, img, lower_bounds, upper_bounds):
+        return cv2.inRange(img, lower_bounds, upper_bounds) # TODO maybe do open / close
+
+    def find_circles(self, mask):
+        res = []
+        contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            (x, y), r = cv2.minEnclosingCircle(contour)
+            if r > 20:
+                res.append((int(x), int(y)))
+        return res
+
+    def close_camera(self):
+        self.camera.close()
+
+    def unzip_list(self, list):
+        res = []
+        for (x, y) in list:
+            res.append(x)
+            res.append(y)
+        return res
+
+
--- src/networking/dataHandler.py
+++ src/networking/dataHandler.py
@@ -13,17 +13,20 @@ class DataHandler:
 
     def on_top_view_data(self, data):
         self.top_view_data = data
-        if self.side_view_data is not None:
-            self.flush()
+        # if self.side_view_data is not None:
+        #     self.flush()
+        self.flush()
 
     def on_side_view_data(self, data):
-        self.side_view_data = data
-        if self.top_view_data is not None:
-            self.flush()
+        # self.side_view_data = data
+        # if self.top_view_data is not None:
+        #     self.flush()
+        pass
 
     def flush(self):
         self.timer.cancel()
-        self.con.update_data(self.top_view_data, self.side_view_data)
+        # self.con.update_data(self.top_view_data, self.side_view_data)
+        self.con.update_data(self.top_view_data, [0,0,0,0,0,0,0,0,0,0])
         self.top_view_data = None
         self.side_view_data = None
         self.timer.start()
