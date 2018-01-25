2d33f6c8359840e9f009c2ea0446b3e1b2c0053f
jelle bakker <j.h.t.bakker@student.utwente.nl>
2018-01-25 15:31:00 +0100

    side view calculations

--- src/control/test.py
+++ src/control/test.py
@@ -8,10 +8,12 @@ pixalcordinates_top = [0,300,450,450,600,300,599,300] # theta1 = (1/4)*pi and se
 
 #  pixalcordinates = [150,450,300,0,450,150,300,600]
 # pixalcordinates_side1 = [0,300,300,200,300,150,300,50,600,300]  # theta2 = 0, theta3 = 0, theta4 = 0
-pixalcordinates_side1 = [0,300,250,250,300,200,350,250,600,300]  # theta2 = 1/2 * pi, theta3 = 0, theta4 = 0
+# pixalcordinates_side1 = [0,300,250,250,300,200,350,250,600,300]  # theta2 = 1/2 * pi, theta3 = 0, theta4 = 0
 # pixalcordinates_side1 = [0,300,300,200,300,150,150,150,600,300]  # theta2 = 0, theta3 = 0, theta4 = -1/2 * pi
 # pixalcordinates_side1 = [0,300,375,225,450,150,525,75,600,300]  # theta2 = 1/4 * pi, theta3 = 0, theta4 = 0
 #pixalcordinates_side1 = [0,300,375,225,300,150,375,75,600,300]  # theta2 = 1/4 * pi, theta3 = -1/2 * pi, theta4 = 1/2 * pi
+pixalcordinates_side1 = [12,65,38,48,56,33,69,48,88,65]  # theta2 = 1/2 * pi, theta3 = 0, theta4 = 0
+
 
 calibration_distance_in_cm = 40
 height_object_in_cm = 5
