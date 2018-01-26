0ab23b2109c23fbb9442ff49af45753d64b32aa2
jelle bakker <j.h.t.bakker@student.utwente.nl>
2018-01-26 09:06:35 +0100

    changed tx ty to cm

--- src/control/statemachine.py
+++ src/control/statemachine.py
@@ -8,10 +8,8 @@ setpoints_initial = [0,0,(1/2)*math.pi,0]
 setpoints_droppoint = [0,(1/4)*math.pi,(1/2)*math.pi,0]
 buffer = 0.1*math.pi
 state_counter = 0
-buffer_endeffector_to_box = 3
-buffer_endeffector_to_droppoint = 3
 
-def state0(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state0(angles, setpoints):
     print("state0 start")
     # delay and decision path to simulate some application logic
     if state_counter == 0:
@@ -25,7 +23,7 @@ def state0(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
     else:
         return None
 
-def state1(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state1(angles, setpoints):
     print("state1 determine angle motor1 for initial setpoint")
     # delay and decision path to simulate some application logic
     error = usbarm.get_error(setpoints_initial, angles)
@@ -34,14 +32,14 @@ def state1(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
     if abs(error[0])<buffer:
         return state3
 
-def state2(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state2(angles, setpoints):
     print("state2 control motor1 for initial setpoint")
     usb_direction =  usbarm.get_usb_direction(setpoints_initial, angles)
 
     usbarm.ctrl(usb_direction[0])
     return None
 
-def state3(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state3(angles, setpoints):
     print("state3 motor1 is fine control angle motor1234")
     # delay and decision path to simulate some application logic
     error = usbarm.get_error(setpoints_initial, angles)
@@ -55,7 +53,7 @@ def state3(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
         usbarm.ctrl(total_movement)
         return None
 
-def state4(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state4(angles, setpoints):
     global state_counter
     print("state4 motor1234 are fine open and close gripper and set state counter to 1")
     usbarm.stop_motors()
@@ -67,7 +65,7 @@ def state4(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
 
 
 
-def state5(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state5(angles, setpoints):
     print("state5 determine rotational setpoint for box")
     error = usbarm.get_error(setpoints, angles)
     if abs(error[0])>=buffer:
@@ -75,18 +73,18 @@ def state5(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
     if abs(error[0])<buffer:
         return state7
 
-def state6(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state6(angles, setpoints):
     print("state6 control motor1 for setpoint")
     usb_direction =  usbarm.get_usb_direction(setpoints, angles)
     usbarm.ctrl(usb_direction[0])
     return None
 
-def state7(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state7(angles, setpoints):
     print("state7 motor1 fine control angle motor234 for the box")
     # delay and decision path to simulate some application logic
     error = usbarm.get_error(setpoints, angles)
 
-    if abs(error[1])<buffer and abs(error[2])< buffer and abs(error[3])< buffer and abs(endeffector_to_box) < buffer_endeffector_to_box:
+    if abs(error[1])<buffer and abs(error[2])< buffer and abs(error[3])< buffer:
         return state8
     else:
         usb_direction = usbarm.get_usb_direction(setpoints, angles)
@@ -94,7 +92,7 @@ def state7(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
         usbarm.ctrl(total_movement)
         return None
 
-def state8(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state8(angles, setpoints):
     global state_counter
     print("state8 control gripper to grab object and set state counter to 2")
     usbarm.stop_motors()
@@ -108,7 +106,7 @@ def state8(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
 
 
 
-def state9(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state9(angles, setpoints):
     print("state9 determine rotational setpoint for box")
     error = usbarm.get_error(setpoints, angles)
     if abs(error[0])>=buffer:
@@ -116,13 +114,13 @@ def state9(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
     if abs(error[0])<buffer:
         return state11
 
-def state10(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state10(angles, setpoints):
     print("state10 control motor1 for setpoint")
     usb_direction =  usbarm.get_usb_direction(setpoints, angles)
     usbarm.ctrl(usb_direction[0])
     return None
 
-def state11(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state11(angles, setpoints):
     print("state11 motor1 fine control angle motor234 for setpoints initial")
     # delay and decision path to simulate some application logic
     new_setpoints = [0]*4
@@ -143,7 +141,7 @@ def state11(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
         usbarm.ctrl(total_movement)
         return None
 
-def state12(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state12(angles, setpoints):
     global state_counter
     print("state12 go to droppoint and set state counter to 3")
     usbarm.stop_motors()
@@ -154,7 +152,7 @@ def state12(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
 
 
 
-def state13(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state13(angles, setpoints):
     print("state13 determine rotational setpoint for droppoint")
     error = usbarm.get_error(setpoints_droppoint, angles)
     if abs(error[0])>=buffer:
@@ -162,18 +160,18 @@ def state13(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
     if abs(error[0])<buffer:
         return state15
 
-def state14(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state14(angles, setpoints):
     print("state14 control motor1 for droppoint")
     usb_direction =  usbarm.get_usb_direction(setpoints_droppoint, angles)
     usbarm.ctrl(usb_direction[0])
     return None
 
-def state15(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state15(angles, setpoints):
     print("state15 motor1 fine control angle motor234 for droppoint")
     # delay and decision path to simulate some application logic
     error = usbarm.get_error(setpoints_droppoint, angles)
 
-    if abs(error[1])<buffer and abs(error[2])< buffer and abs(error[3])< buffer and abs(endeffector_to_droppoint) < buffer_endeffector_to_droppoint:
+    if abs(error[1])<buffer and abs(error[2])< buffer and abs(error[3])< buffer:
         return state16
     else:
         usb_direction = usbarm.get_usb_direction(setpoints_droppoint, angles)
@@ -181,7 +179,7 @@ def state15(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
         usbarm.ctrl(total_movement)
         return None
 
-def state16(angles, setpoints, endeffector_to_box, endeffector_to_droppoint):
+def state16(angles, setpoints):
     global state_counter
     print("state16 open gripper and set state counter to 0")
     usbarm.stop_motors()
