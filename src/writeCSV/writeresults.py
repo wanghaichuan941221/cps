1dcd72fe8c955a5385ab9bcbc0ca2d1f6ab194c0
jelle bakker <j.h.t.bakker@student.utwente.nl>
2018-01-29 16:04:19 +0100

    csv test removed test for control

--- src/writeCSV/writeresults.py
+++ src/writeCSV/writeresults.py
@@ -2,6 +2,7 @@ import csv
 import time
 
 need_to_write = True
+time_at_start = time.time()
 
 def write_to_csv_intial_motor1():
     if need_to_write:
@@ -28,7 +29,7 @@ def write_to_csv_motor1(setpoint1, theta1):
         with myFile:
             myFields = ['time', 'setpoint_1', 'theta_1']
             writer = csv.DictWriter(myFile, fieldnames=myFields)
-            cur_time = time.time()
+            cur_time = time.time()-time_at_start
             writer.writerow({'time': cur_time, 'setpoint_1': setpoint1, 'theta_1': theta1} )
         myFile.close()
 
@@ -38,7 +39,7 @@ def write_to_csv_motor234(setpoint234, theta234):
         with myFile:
             myFields = ['time', 'setpoint_2', 'setpoint_3', 'setpoint_4', 'theta_2', 'theta_3', 'theta_4']
             writer = csv.DictWriter(myFile, fieldnames=myFields)
-            cur_time = time.time()
+            cur_time = time.time()-time_at_start
             writer.writerow({'time': cur_time, 'setpoint_2': setpoint234[0], 'setpoint_3': setpoint234[1], 'setpoint_4': setpoint234[2], 'theta_2': theta234[0], 'theta_3': theta234[1], 'theta_4': theta234[2]} )
         myFile.close()
 
