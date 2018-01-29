5ba2d640ef03a485260683634932e560ed5eb345
jelle bakker <j.h.t.bakker@student.utwente.nl>
2018-01-29 14:39:46 +0100

    csv test removed test for control

--- src/writeCSV/writeresults.py
+++ src/writeCSV/writeresults.py
@@ -7,12 +7,12 @@ angles = ['5', '6', '7', '8']
 def write_to_csv(setpoint, angle):
     myFile = open('results.csv', 'w')
     with myFile:
-        myFields = ['setpoints', 'angles', 'time']
+        myFields = ['time', 'setpoints', 'angles']
         writer = csv.DictWriter(myFile, fieldnames=myFields)
         writer.writeheader()
 
     cur_time = time.time()
-    writer.writerow({'time': cur_time, 'setpoints': setpoint, 'angles': angle} )
+    writer.writerow({'time': cur_time, 'setpoints': setpoints[1], 'angles': angles[1]} )
 
 def read_results():
     with open('results.csv') as myFile:
