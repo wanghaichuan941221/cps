4d7f78749ee34d3c37b42a7f9f7fb7edaf791db4
jelle bakker <j.h.t.bakker@student.utwente.nl>
2018-01-29 13:45:09 +0100

    csv test removed test for control

--- src/writeCSV/testcsv.py
+++ src/writeCSV/testcsv.py
@@ -1,17 +1,21 @@
 import csv
+import time
 
 myFile = open('countries.csv', 'w')
+countries = [France, Italy, Spain, Russia]
+capital = [Paris, Rome, Madrid, Moscow]
+
 with myFile:
     myFields = ['country', 'capital']
     writer = csv.DictWriter(myFile, fieldnames=myFields)
     writer.writeheader()
-    writer.writerow({'country' : 'France', 'capital': 'Paris'})
-    writer.writerow({'country' : 'Italy', 'capital': 'Rome'})
-    writer.writerow({'country' : 'Spain', 'capital': 'Madrid'})
-    writer.writerow({'country' : 'Russia', 'capital': 'Moscow'})
+
+    for i in range(0,len(countries)):
+        writer.writerow({'country' : countries[i], 'capital': capital[i]})
 
 
 with open('countries.csv') as myFile:
     reader = csv.DictReader(myFile)
     for row in reader:
         print(row['country'])
+        print(row['capital'])
